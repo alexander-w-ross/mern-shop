1c8e385bad3665fd1dc4f862d6de776453a47442
Alex Ross <alexander.w.rosss@gmail.com>
2020-04-30 14:13:45 -0400

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const items = require("./routes/api/items");
 
@@ -22,6 +23,17 @@ mongoose
 
 // Use Routes
 app.use("/api/items", items);
+
+// Server static assets if in prod
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  // load frontend build path unless its hitting /api/items
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
