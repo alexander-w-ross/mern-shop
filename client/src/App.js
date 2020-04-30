e3f50bf6b20582f517bd53fcf86aefe9b4cb575c
Alex Ross <alexander.w.rosss@gmail.com>
2020-04-30 13:43:30 -0400

    deleted unused variables

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ import ShoppingList from "./components/ShoppingList";
 import { Provider } from "react-redux";
 import store from "./store";
 import ItemModal from "./components/ItemModal";
-import { Container } from "reactstrap";
 
 function App() {
   return (
